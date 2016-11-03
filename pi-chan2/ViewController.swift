475beff2d93f3781615e7313762fbf4beb86aef7
starhoshi <kensuke1751@gmail.com>
2016-11-04 00:52:23 +0900

    Initial Commit

---
+++ pi-chan2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pi-chan2
+//
+//  Created by Kensuke Hoshikawa on 2016/11/04.
+//  Copyright © 2016年 star__hoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
