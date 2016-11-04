1bae829f2b33f43bdbe86e9f339a563f4d20defe
starhoshi <kensuke1751@gmail.com>
2016-11-04 10:40:30 +0900

    初期ファイル諸々追加

---
+++ pi-chan2/Extensions/URL+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  URL+Extension.swift
+//  pi-chan2
+//
+//  Created by Kensuke Hoshikawa on 2016/11/04.
+//  Copyright © 2016年 star__hoshi. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    static func defaultUrl() -> URL {
+        return URL(string: "http://")!
+    }
+}
---
+++ pi-chan2/Utils/Log.swift
@@ -0,0 +1,41 @@
+//
+//  LOg.swift
+//  pi-chan2
+//
+//  Created by Kensuke Hoshikawa on 2016/11/04.
+//  Copyright © 2016年 star__hoshi. All rights reserved.
+//
+
+import Foundation
+import XCGLogger
+
+let log: XCGLogger? = {
+    #if DEBUG
+        let logger = XCGLogger.default
+        logger.setup(level: .verbose, showThreadName: true, showLevel: true, showFileNames: true, showLineNumbers: true)
+
+        if let fileDestination: FileDestination = logger.destination(withIdentifier: XCGLogger.Constants.fileDestinationIdentifier) as? FileDestination {
+            let ansiColorLogFormatter: ANSIColorLogFormatter = ANSIColorLogFormatter()
+            ansiColorLogFormatter.colorize(level: .verbose, with: .colorIndex(number: 244), options: [.faint])
+            ansiColorLogFormatter.colorize(level: .debug, with: .black)
+            ansiColorLogFormatter.colorize(level: .info, with: .blue, options: [.underline])
+            ansiColorLogFormatter.colorize(level: .warning, with: .red, options: [.faint])
+            ansiColorLogFormatter.colorize(level: .error, with: .red, options: [.bold])
+            ansiColorLogFormatter.colorize(level: .severe, with: .white, on: .red)
+            fileDestination.formatters = [ansiColorLogFormatter]
+        }
+
+        let emojiLogFormatter = PrePostFixLogFormatter()
+        emojiLogFormatter.apply(prefix: "🗯🗯🗯 ", postfix: " 🗯🗯🗯", to: .verbose)
+        emojiLogFormatter.apply(prefix: "🔹🔹🔹 ", postfix: " 🔹🔹🔹", to: .debug)
+        emojiLogFormatter.apply(prefix: "ℹ️ℹ️ℹ️ ", postfix: " ℹ️ℹ️ℹ️", to: .info)
+        emojiLogFormatter.apply(prefix: "⚠️⚠️⚠️ ", postfix: " ⚠️⚠️⚠️", to: .warning)
+        emojiLogFormatter.apply(prefix: "‼️‼️‼️ ", postfix: " ‼️‼️‼️", to: .error)
+        emojiLogFormatter.apply(prefix: "💣💣💣 ", postfix: " 💣💣💣", to: .severe)
+        logger.formatters = [emojiLogFormatter]
+
+        return logger
+    #else
+        return nil
+    #endif
+}()
